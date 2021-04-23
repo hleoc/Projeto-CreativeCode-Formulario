69daf48db8d53065ab9a222e0ed69693caf01912
hleoc <informig@gmail.com>
2021-04-23 10:57:36 -0300

    Criação da pasta controllers e do arquivo UserController.ts

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from 'express';
+
+function validateEmail(email: string) {
+  const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return regexEmail.test(String(email).toLowerCase());
+}
+
+class UserController {
+  public async create(req: Request, res: Response) {
+    try {
+      const { nome, telefone, email, idade, peso, senha, etinia } = req.body;
+      if (!validateEmail(email)) {
+        return res.status(401).json({ message: 'Email inválido' });
+      }
+      const newUser = await createUser(nome, telefone, email, idade, peso, senha, etinia);
+      return res.status(201).json(newUser);
+    } catch (error) {
+      return res.status(500).json({ message: 'Algo deu errado.' });
+    }
+  }
+
+  public async index(req: Request, res: Response) {
+    try {
+      const users = await findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.status(500).json({ message: 'Algo deu errado.' });
+    }
+  }
+
+  public async indexById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const users = await findOne(id);
+      return res.status(200).json(users);
+    } catch (error) {
+      return res.status(500).json({ message: 'Algo deu errado.' });
+    }
+  }
+
+  public async update(req: Request, res: Response) {
+    try {
+      const { nome, telefone, email, idade, peso, senha, etinia } = req.body;
+      if (!validateEmail(email)) {
+        return res.status(401).json({ message: 'Email inválido' });
+      }
+      const editUser = await updateUser(nome, telefone, email, idade, peso, senha, etinia);
+      return res.status(201).json(editUser);
+    } catch (error) {
+      return res.status(500).json({ message: 'Algo deu errado.' });
+    }
+  }
+
+  public async remove(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const users = await removeUser(id);
+      return res.status(200).json(users);
+    } catch (error) {
+      return res.status(500).json({ message: 'Algo deu errado.' });
+    }
+  }
+  
+}
+
+export default new UserController();
