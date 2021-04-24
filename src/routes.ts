2b266147aeb041736469ab94b331704aad533277
hleoc <informig@gmail.com>
2021-04-23 21:02:07 -0300

    Autenticação das Rotas

--- src/routes.ts
+++ src/routes.ts
@@ -2,21 +2,21 @@ import { Router } from 'express';
 import UserController from './controllers/UserController';
 import EnderecoController from './controllers/EnderecoController';
 import LoginController from './controllers/LoginController';
+import Auth from './middleware/Auth';
 
 const routes = Router();
 
 routes.post('/users/create', UserController.create);
-routes.get('/users', UserController.index);
-routes.get('/users/:id', UserController.indexById);
-routes.put('/users/update', UserController.update);
-routes.delete('/users/delete/:id', UserController.remove);
 routes.post('/users/login', LoginController.login);
+routes.get('/users', Auth, UserController.index);
+routes.get('/users/:id', Auth, UserController.indexById);
+routes.put('/users/update', Auth, UserController.update);
+routes.delete('/users/delete/:id', Auth, UserController.remove);
 
-routes.post('/endereco/create', EnderecoController.create);
-routes.get('/endereco', EnderecoController.index);
-routes.get('/endereco/:id', EnderecoController.indexById);
-routes.put('/endereco/update', EnderecoController.update);
-routes.delete('/endereco/delete/:id', EnderecoController.remove);
-
+routes.post('/endereco/create', Auth, EnderecoController.create);
+routes.get('/endereco', Auth, EnderecoController.index);
+routes.get('/endereco/:id', Auth, EnderecoController.indexById);
+routes.put('/endereco/update', Auth, EnderecoController.update);
+routes.delete('/endereco/delete/:id', Auth, EnderecoController.remove);
 
 export default routes;
